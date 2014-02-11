d9ec0815417c2f773b6bc5ddb13b411687b2faba
SkylerTyler1337 <tylernichols409@gmail.com>
2014-02-11 10:48:05 -0600

    Update JoinCommand.java

--- me/skylertyler1337/teamplugin/Commands/JoinCommand.java
+++ me/skylertyler1337/teamplugin/Commands/JoinCommand.java
@@ -1,5 +1,20 @@
 package me.skylertyler1337.teamplugin.Commands;
 
+import org.bukkit.command.CommandExecutor;
+
 
 public class JoinCommand implements CommandExecutor{
+
+
+	public TeamPlugin plugin;
+
+	public ArcherCommand(TeamPlugin instance) {
+		return;
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String commandlabel
+			,String[] args) {
+		Player p = (Player) sender;
+		if(commandlabel.equalsIgnoreCase("join"));
+
 }
