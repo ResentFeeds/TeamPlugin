8db499ea6b4c5e20184158187bb65a02599ea02c
SkylerTyler1337 <tylernichols409@gmail.com>
2014-02-11 08:39:52 -0600

    Forgot to add the class
    import org.bukkit.plugin.java.JavaPlugin;
    
    public class KitsPlugin extends JavaPlugin{

--- me/skylertyler1337/teamplugin/TeamPlugin.java
+++ me/skylertyler1337/teamplugin/TeamPlugin.java
@@ -1,5 +1,12 @@
 package me.skylertyler1337.teamplugin;
 
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+   public class TeamPlugin extends JavaPlugin{
+
+
+
 	@Override
 	public void onEnable() {
 	  System.out.println("[TeamPlugin] is Enabled!");
