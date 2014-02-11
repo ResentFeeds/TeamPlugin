659d139fdd9c645d626c128eccf681e591e37286
SkylerTyler1337 <tylernichols409@gmail.com>
2014-02-11 17:54:02 -0600

    done with Main class

--- me/skylertyler1337/teamplugin/TeamPlugin.java
+++ me/skylertyler1337/teamplugin/TeamPlugin.java
@@ -1,25 +1,27 @@
 package me.skylertyler1337.teamplugin;
 
-import me.skylertyler1337.teamplugin.Commands.*;
+import me.skylertyler1337.teamplugin.Commands.JoinCommand;
+import me.skylertyler1337.teamplugin.Commands.MyteamCommand;
+import me.skylertyler1337.teamplugin.Teams.Team;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-   public class TeamPlugin extends JavaPlugin{
+public class TeamPlugin extends JavaPlugin {
 
-
-
+	
 	@Override
 	public void onEnable() {
-	  System.out.println("[TeamPlugin] is Enabled!");
-          getCommand("join").setExecutor(new JoinCommand(this));
+	System.out.println("[TeamPlugin] is Enabled!");
+	getCommand("join").setExecutor(new JoinCommand(this));
+	getCommand("myteam").setExecutor(new MyteamCommand(this));
+	Team.ClearTeams();
 	}
-       
-        @Override
-         public void onDisable () {
-          System.out.println("[TeamPlugin] is Disabled!"); 
-      }
-      // will work on later!
+	
+	@Override
+	public void onDisable() {
+	System.out.println("[TeamPlugin] is Disabled!");
+	Team.ClearTeams();
+	}
+	
 }
 
-       
-     
