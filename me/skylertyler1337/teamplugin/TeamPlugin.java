43a2398553c339e34992bd7b13719fb53900c811
SkylerTyler1337 <tylernichols409@gmail.com>
2014-02-10 22:44:51 -0600

    added main class
    will work later tomorrow.

---
+++ me/skylertyler1337/teamplugin/TeamPlugin.java
@@ -0,0 +1,16 @@
+package me.skylertyler1337.teamplugin;
+
+	@Override
+	public void onEnable() {
+	  System.out.println("[TeamPlugin] is Enabled!");
+	}
+       
+        @Override
+         public void onDisable () {
+          System.out.println("[TeamPlugin] is Disabled!"); 
+      }
+      // will work on later!
+}
+
+       
+     
