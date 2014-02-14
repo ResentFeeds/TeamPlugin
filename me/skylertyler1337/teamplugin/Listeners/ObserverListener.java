176d139caccbe7e5ddc603d75e534f3497267446
SkylerTyler1337 <tylernichols409@gmail.com>
2014-02-14 10:08:56 -0600

    Made the base

--- me/skylertyler1337/teamplugin/Listeners/ObserverListener.java
+++ me/skylertyler1337/teamplugin/Listeners/ObserverListener.java
@@ -1,1 +1,9 @@
-will work on later!
+package me.skylertyler1337.teamplugin.Listeners;
+
+   public class ObserverListener implements Listener{
+    TeamPlugin instance;
+
+  public void ObserverListener(TeamPlugin instance){
+    this.plugin = instance;
+    }
+  }
