8f6a168a24e0a82f607287eb49d4c58ab487ea0e
SkylerTyler1337 <tylernichols409@gmail.com>
2014-03-13 10:33:49 -0500

    Forgetting imports

--- me/skylertyler1337/teamplugin/Listeners/ObserverListener.java
+++ me/skylertyler1337/teamplugin/Listeners/ObserverListener.java
@@ -1,4 +1,5 @@
 package me.skylertyler1337.teamplugin.Listeners;
+import me.skylertyler1337.teamplugin.TeamPlugin;
 import org.bukkit.event.Listener;
    public class ObserverListener implements Listener{
  public TeamPlugin instance;
