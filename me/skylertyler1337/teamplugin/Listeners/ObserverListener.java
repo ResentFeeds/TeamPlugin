2ab74c9bdf0f6ef29b85d48a7c6ec03dfbdf1222
SkylerTyler1337 <tylernichols409@gmail.com>
2014-03-13 10:31:28 -0500

    Made this Line public

--- me/skylertyler1337/teamplugin/Listeners/ObserverListener.java
+++ me/skylertyler1337/teamplugin/Listeners/ObserverListener.java
@@ -1,7 +1,7 @@
 package me.skylertyler1337.teamplugin.Listeners;
 import org.bukkit.event.Listener;
    public class ObserverListener implements Listener{
-    TeamPlugin instance;
+ public TeamPlugin instance;
 
   public void ObserverListener(TeamPlugin instance){
     this.plugin = instance;
