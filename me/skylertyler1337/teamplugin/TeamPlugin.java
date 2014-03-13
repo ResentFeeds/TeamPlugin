8a3271ca7f49d6bbfbe92f453579c39a196f113c
SkylerTyler1337 <tylernichols409@gmail.com>
2014-03-13 18:07:57 -0500

    Update TeamPlugin.java

--- me/skylertyler1337/teamplugin/TeamPlugin.java
+++ me/skylertyler1337/teamplugin/TeamPlugin.java
@@ -1,5 +1,9 @@
 package me.skylertyler1337.teamplugin;
 
+import me.skylertyler1337.teamplugin.Listeners.EntityDamageListener;
+import me.skylertyler1337.teamplugin.Listeners.ObserverListener;
+import me.skylertyler1337.teamplugin.Listeners.ChatListener;
+import me.skylertyler1337.teamplugin.Listeners.PickTeamListener;
 import me.skylertyler1337.teamplugin.Commands.JoinCommand;
 import me.skylertyler1337.teamplugin.Commands.MyteamCommand;
 import me.skylertyler1337.teamplugin.Teams.Team;
@@ -12,6 +16,10 @@ public class TeamPlugin extends JavaPlugin {
 	@Override
 	public void onEnable() {
 	System.out.println("[TeamPlugin] is Enabled!");
+  //still working on this getServer().getPluginManager().registerEvents(new EntityDamageListener(this),  this);
+	getServer().getPluginManager().registerEvents(new ObserverListener(this), this);
+ //still working on this getServer().getPluginManager().registerEvents(new ChatListener(this), this);
+	getServer().getPluginManager().registerEvents(new PickTeamListener(this), this);
 	getCommand("join").setExecutor(new JoinCommand(this));
 	getCommand("myteam").setExecutor(new MyteamCommand(this));
 	Team.ClearTeams();
